4b8a1c727f8072cdb3f2e0bdd17870e14424ccdc
Varshith Pawar H R <varshithpawarhr@gmail.com>
2025-05-18 22:40:55 +0530

    feat:glitch removed

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,13 +5,24 @@ export default {
     extend: {
       keyframes: {
         scaleAnim: {
-          '0%': { transform: 'scale(1)' },
-          '50%': { transform: 'scale(1.1)' },
-          '100%': { transform: 'scale(1)' },
+          "0%": { transform: "scale(1)" },
+          "50%": { transform: "scale(1.1)" },
+          "100%": { transform: "scale(1)" },
+        },
+        orbit: {
+          "0%": {
+            transform:
+              "rotate(0deg) translateY(calc(var(--radius) * 1px)) rotate(0deg)",
+          },
+          "100%": {
+            transform:
+              "rotate(360deg) translateY(calc(var(--radius) * 1px)) rotate(-360deg)",
+          },
         },
       },
       animation: {
-        scale: 'scaleAnim 300ms ease-in-out',
+        scale: "scaleAnim 300ms ease-in-out",
+        orbit: "orbit calc(var(--duration)*1s) linear infinite",
       },
     },
   },
