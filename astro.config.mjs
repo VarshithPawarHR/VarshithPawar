db7b1334139c3b5701f2d7e88fbe904dc7df6eed
Varshith Pawar H R <varshithpawarhr@gmail.com>
2025-05-18 17:43:47 +0530

    bug:fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import vercelStatic from '@astrojs/vercel/static';
 
 import react from "@astrojs/react";
+
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+
   vite: {
     resolve: {
       alias: {
@@ -15,12 +18,17 @@ export default defineConfig({
       },
     },
   },
-  output: "static",
+
+
   build: {
     inlineStylesheets: "auto",
   },
+
   server: {
     host: true,
     port: 4321,
   },
+ output: 'static',
+// @ts-ignore
+adapter: vercelStatic(),
 });
